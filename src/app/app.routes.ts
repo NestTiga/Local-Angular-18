420dc47ef985e2b16b4b646464a4e293a470f217
NestTiga <nestortigasi371@gmail.com>
2024-07-30 13:20:10 -0500

    Creado componentes para gestión de clientes e inicio de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListCustomerComponent } from './components/local-components/customers/list-customer/list-customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'customers', component: ListCustomerComponent}
+];
